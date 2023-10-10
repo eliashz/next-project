2b2e7f087daaa1756462fdf33665b414ad1a6fbd
eliashz <eliashz@gmail.com>
2023-10-10 11:44:48 +0200

    Styles

--- app/page.js
+++ app/page.js
@@ -7,15 +7,16 @@ const fetchUsers = async url => {
 const HomePage = async () => {
   const users = await fetchUsers("https://reqres.in/api/users");
   return (
-    <ul className="">
+    <ul>
       {users.map(user => (
-        <li key={user.id}>
+        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md flex text-black justify-between">
           <div>
-            <h5>
+            <h5 className="font-bold">
               {user.id} {user.first_name} {user.last_name}
             </h5>
-            <p>email: {user.email}</p>
+            <p className="text-slate-100">email: {user.email}</p>
           </div>
+          <img src={user.avatar} alt="" className="rounded-full w-20" />
         </li>
       ))}
     </ul>
