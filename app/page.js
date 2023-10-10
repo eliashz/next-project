326af6f0b056d5154682edab58c703378b81c343
eliashz <eliashz@gmail.com>
2023-10-10 13:20:14 +0200

    Creando Users

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Users from "@/component/Users";
+
 const fetchUsers = async url => {
   const res = await fetch(url);
   const data = await res.json();
@@ -6,21 +8,9 @@ const fetchUsers = async url => {
 
 const HomePage = async () => {
   const users = await fetchUsers("https://reqres.in/api/users");
-  return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id} className="bg-slate-400 mb-2 p-4 rounded-md flex text-black justify-between">
-          <div>
-            <h5 className="font-bold">
-              {user.id} {user.first_name} {user.last_name}
-            </h5>
-            <p className="text-slate-100">email: {user.email}</p>
-          </div>
-          <img src={user.avatar} alt="" className="rounded-full w-20" />
-        </li>
-      ))}
-    </ul>
-  );
+
+  return <Users users={users} />;
 };
 
 export default HomePage;
+ 
