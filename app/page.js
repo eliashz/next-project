a45126339dd44649eae4a79e56514abb4541ec61
eliashz <eliashz@gmail.com>
2023-10-10 11:36:01 +0200

    fetch users

--- app/page.js
+++ app/page.js
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>Home Page</div>;
+const fetchUsers = async url => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.data;
+};
+
+const HomePage = async () => {
+  const users = await fetchUsers("https://reqres.in/api/users");
+  return (
+    <ul className="">
+      {users.map(user => (
+        <li key={user.id}>
+          <div>
+            <h5>
+              {user.id} {user.first_name} {user.last_name}
+            </h5>
+            <p>email: {user.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default HomePage;
